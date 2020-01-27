4b3fe80ed6e9584536638e1751bd2e99033655a7
Kevin Paton <kevin.paton@cgi.com>
2020-01-27 17:13:18 +0000

    Todo

--- src/main/java/com/example/sonar/sonardemo/example/Example.java
+++ src/main/java/com/example/sonar/sonardemo/example/Example.java
@@ -1,16 +1,24 @@
 package com.example.sonar.sonardemo.example;
 
+import java.security.PrivilegedAction;
+
 public class Example {
 
     private String string;
 
     public Example() {
         infiniteLoop();
+        System.out.println("USERNAME=" + getSystemProperty("USERNAME"));
     }
 
     private void infiniteLoop() {
         while (1 == 1) {
             // Do nothing
         }
+    }
+
+    private String getSystemProperty(String string) {
+        final PrivilegedAction<String> privilegedAction = () -> System.getProperty(string);
+        return privilegedAction.run();
     }
 }
